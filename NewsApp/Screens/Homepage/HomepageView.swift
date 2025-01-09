2599c116353fc7912ed939df679bd325cc7dadc7
Burak Özdemir <mburak.ozdemir03@gmail.com>
2025-01-09 23:04:38 +0300

    Add: homepage view ui design completed.

--- NewsApp/Screens/Homepage/HomepageView.swift
+++ NewsApp/Screens/Homepage/HomepageView.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 //MARK: - HomepageView
 
@@ -13,23 +14,83 @@ class HomepageView: UIViewController {
 
     //MARK: - Properties
 
+    private let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "Type a text"
+        searchBar.keyboardType = .default
+        searchBar.showsCancelButton = false
+        searchBar.searchBarStyle = .default
+        return searchBar
+    }()
     
+    private let newsTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.rowHeight = 200
+        tableView.separatorStyle = .none
+        tableView.showsHorizontalScrollIndicator = false
+        tableView.showsVerticalScrollIndicator = true
+        return tableView
+    }()
     
     //MARK: - Life Cycles
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        newsTableView.delegate = self
+        newsTableView.dataSource = self
+        searchBar.delegate = self
+        
+        configureUI()
     }
     
     //MARK: - Private Methods
 
+    private func addViews() {
+        view.addSubview(searchBar)
+        view.addSubview(newsTableView)
+    }
+    
+    private func configureConstraints() {
+        searchBar.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+        }
+        
+        newsTableView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.top.equalTo(searchBar.snp.bottom)
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
+    }
+    
+    private func configureUI() {
+        navigationItem.title = "Appcent News App"
+        addViews()
+        configureConstraints()
+    }
+}
+
+//MARK: - UITableViewDataSource
+
+extension HomepageView: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
 
 //MARK: - UITableViewDelegate
 
+extension HomepageView: UITableViewDelegate {
+    
+}
 
+//MARK: - UISearchBarDelegate
 
-//MARK: - UITableViewDataSource
-
-
+extension HomepageView: UISearchBarDelegate {
+    
+}
