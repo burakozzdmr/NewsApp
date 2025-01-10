312f343f73790763cfe577d8878deab62f753e64
Burak Özdemir <mburak.ozdemir03@gmail.com>
2025-01-10 14:18:30 +0300

    Add: viewmodel logic process created.

--- NewsApp/Screens/Homepage/HomepageView.swift
+++ NewsApp/Screens/Homepage/HomepageView.swift
@@ -75,11 +75,11 @@ class HomepageView: UIViewController {
 
 extension HomepageView: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        
     }
 }
 
--- NewsApp/Screens/Homepage/HomepageViewModel.swift
+++ NewsApp/Screens/Homepage/HomepageViewModel.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+//MARK: - Protocols
+
+protocol HomepageViewModelProtocol: AnyObject {
+    func didReceiveData(_ data: [News])
+}
+
+//MARK: - HomepageViewModel
+
+class HomepageViewModel {
+    weak var delegate: HomepageViewModelProtocol?
+    var networkData: [News] = []
+    var filteredData: [News] = []
+    
+    init() {
+        NetworkManager.shared.getData { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let data):
+                self.networkData = data.articles ?? []
+                delegate?.didReceiveData(networkData)
+            case .failure(let error):
+                print("View Model Error: \(error.localizedDescription)")
+            }
+            
+        }
+    }
+}
